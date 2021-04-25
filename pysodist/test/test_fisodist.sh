485ebdaf269eeb12941c97595fec01740f80bdd3
jhdavis <32502527+jhdavislab@users.noreply.github.com>
2021-04-25 14:37:31 -0400

    completed testing

--- pysodist/test/test_fisodist.sh
+++ pysodist/test/test_fisodist.sh
@@ -4,5 +4,4 @@ set -x
 pysodist_dir=${PWD%/*}
 base_dir=${pysodist_dir%/*}
 pysodist run_isodist ./test_result/raw_spec/Glu_15N_300_DIA/pd_exported_peaks.tsv $base_dir/fortran/isodist $base_dir/model_files/atoms.txt $base_dir/model_files/U_var500N_fix998N.txt --threads 30 --wait_time 10 --pysodist_input ./test_result/raw_spec/Glu_15N_300_DIA/pd_parsed_report.tsv --logfile test_fisodist.log
-
 pysodist run_isodist ./test_result/sum_spec/Glu_15N_300_DIA/pd_exported_peaks.tsv $base_dir/fortran/isodist $base_dir/model_files/atoms.txt $base_dir/model_files/U_var500N_fix998N.txt --threads 2 --wait_time 10 --pysodist_input ./test_result/sum_spec/Glu_15N_300_DIA/pd_parsed_report.tsv --no_compress --logfile test_fisodist.log
